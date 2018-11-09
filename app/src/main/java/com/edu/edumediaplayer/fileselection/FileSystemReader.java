12cf8b2f2af972c934d19458247c3e0f5a51afeb
Alexandru Olteanu <alex.c.olteanu@gmail.com>
2018-11-09 13:52:03 +0200

    caching mp3s from the MediaStore in a HashSet and in a FileSystemTree

--- app/src/main/java/com/edu/edumediaplayer/fileselection/FileSystemReader.java
+++ app/src/main/java/com/edu/edumediaplayer/fileselection/FileSystemReader.java
@@ -1,15 +1,28 @@
 package com.edu.edumediaplayer.fileselection;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 
 public class FileSystemReader {
 
+    Activity activity;
+    Set<String> mp3sOnDevice;
+    FileSystemTree fsTree;
+
     public FileSystemReader(Activity activity) {
+        this.activity = activity;
         PermissionObtainer.obtainPermissions(activity);
     }
 
@@ -20,5 +33,26 @@ public class FileSystemReader {
             }
         });
         return files;
+    }
+
+    public void cacheMp3sOnDevice() {
+        ContentResolver contentResolver = activity.getContentResolver();
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selectionMimeType = MediaStore.Files.FileColumns.MIME_TYPE + "=?";
+        String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension("mp3");
+        String[] selectionArgsMp3 = new String[]{ mimeType };
+        Cursor songCursor = contentResolver.query(songUri, null, selectionMimeType, selectionArgsMp3, null);
+        mp3sOnDevice = new HashSet<>();
+        fsTree = new FileSystemTree();
+
+        if (songCursor!=null && songCursor.moveToFirst()){
+            int songPath = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
+
+            do {
+                String currentPath = songCursor.getString(songPath);
+                mp3sOnDevice.add(currentPath);
+                fsTree.addFile(currentPath);
+            } while (songCursor.moveToNext());
+        }
     }
 }
---
+++ app/src/main/java/com/edu/edumediaplayer/fileselection/FileSystemTree.java
@@ -0,0 +1,12 @@
+package com.edu.edumediaplayer.fileselection;
+
+public class FileSystemTree {
+
+    public FileSystemTree() {
+
+    }
+
+    public void addFile(String filePath) {
+    }
+
+}
