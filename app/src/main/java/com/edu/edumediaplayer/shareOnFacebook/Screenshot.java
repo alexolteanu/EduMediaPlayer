59d1fc7b1b77ae73690901315ebd2b90383d08d7
Poppy22 <carmengpopa13@gmail.com>
2018-12-31 12:52:22 +0200

    Finally a working version

--- app/src/main/java/com/edu/edumediaplayer/shareOnFacebook/Screenshot.java
+++ app/src/main/java/com/edu/edumediaplayer/shareOnFacebook/Screenshot.java
@@ -5,12 +5,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 
 import com.edu.edumediaplayer.BuildConfig;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -27,15 +29,23 @@ public class Screenshot {
         return screenshot;
     }
 
-    public static String storeScreenshot(Bitmap screenshot, String filename) {
-        String path = Environment.getExternalStorageDirectory().toString() + "/" + filename;
-        OutputStream out = null;
+    public static String storeScreenshot(Bitmap bitmap, String filename) {
+
+        Long tsLong = System.currentTimeMillis() / 1000;
+        String timeStamp = tsLong.toString();
+
+        String path = Environment.getExternalStorageDirectory().toString() + "/" +
+                filename + timeStamp + ".jpg";
         File imageFile = new File(path);
 
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        FileOutputStream fo = null;
+
         try {
-            out = new FileOutputStream(imageFile);
-            screenshot.compress(Bitmap.CompressFormat.JPEG, 99, out);
-            out.flush();
+            imageFile.createNewFile();
+            fo = new FileOutputStream(imageFile);
+            fo.write(bytes.toByteArray());
 
         } catch (FileNotFoundException e) {
             Log.i("Exception:", "File not found.");
@@ -46,8 +56,8 @@ public class Screenshot {
         } finally {
 
             try {
-                if (out != null) {
-                    out.close();
+                if (fo != null) {
+                    fo.close();
                     return imageFile.toString();
                 }
 
